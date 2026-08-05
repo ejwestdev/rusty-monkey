12337fb31303af0cc5737c0290db02e4afe41f51
Edward West <edwardwestdev@gmail.com>
2026-08-05 15:59:51 -0700

    extended tests

--- src/lexer/lexer_test.rs
+++ src/lexer/lexer_test.rs
@@ -4,15 +4,49 @@ mod tests {
     use crate::lexer::token::TokenType;
     #[test]
     fn test_next_token() {
-        let input = "=+(){},;";
+        let input = "let five = 5;
+let ten = 10;
+let add = fn(x,  y) {
+    x + y;
+};
+
+let result = add(five, ten);";
         let cases = vec![
+            (TokenType::Let, "let".to_string()),
+            (TokenType::Ident("five".to_string()), "five".to_string()),
             (TokenType::Assign, "=".to_string()),
-            (TokenType::Plus, "+".to_string()),
+            (TokenType::Int("5".to_string()), "5".to_string()),
+            (TokenType::Semicolon, ";".to_string()),
+            (TokenType::Let, "let".to_string()),
+            (TokenType::Ident("ten".to_string()), "ten".to_string()),
+            (TokenType::Assign, "=".to_string()),
+            (TokenType::Int("10".to_string()), "10".to_string()),
+            (TokenType::Semicolon, ";".to_string()),
+            (TokenType::Let, "let".to_string()),
+            (TokenType::Ident("add".to_string()), "add".to_string()),
+            (TokenType::Assign, "=".to_string()),
+            (TokenType::Function, "fn".to_string()),
             (TokenType::Lparen, "(".to_string()),
+            (TokenType::Ident("x".to_string()), "x".to_string()),
+            (TokenType::Comma, ",".to_string()),
+            (TokenType::Ident("y".to_string()), "y".to_string()),
             (TokenType::Rparen, ")".to_string()),
             (TokenType::Lbrace, "{".to_string()),
+            (TokenType::Ident("x".to_string()), "x".to_string()),
+            (TokenType::Plus, "+".to_string()),
+            (TokenType::Ident("y".to_string()), "y".to_string()),
+            (TokenType::Semicolon, ";".to_string()),
             (TokenType::Rbrace, "}".to_string()),
+            (TokenType::Semicolon, ";".to_string()),
+            (TokenType::Let, "let".to_string()),
+            (TokenType::Ident("result".to_string()), "result".to_string()),
+            (TokenType::Assign, "=".to_string()),
+            (TokenType::Ident("add".to_string()), "add".to_string()),
+            (TokenType::Lparen, "(".to_string()),
+            (TokenType::Ident("five".to_string()), "five".to_string()),
             (TokenType::Comma, ",".to_string()),
+            (TokenType::Ident("ten".to_string()), "ten".to_string()),
+            (TokenType::Rparen, ")".to_string()),
             (TokenType::Semicolon, ";".to_string()),
             (TokenType::Eof, "".to_string()),
         ];

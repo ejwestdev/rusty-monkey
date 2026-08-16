8d1e1f4d2bac4e888aca6e45aaf28936464b2d40
Edward West <edwardwestdev@gmail.com>
2026-08-16 14:36:39 -0700

    upgraded the RLPL (read-lex-print-loop) to RPPL (read-parse-print-loop)

--- src/repl.rs
+++ src/repl.rs
@@ -1,9 +1,19 @@
 use crate::lexing::lexer_impl::Lexer;
+use crate::parsing::parser::Parser;
 use crate::token::TokenType;
 use std::io::BufRead;
 use std::io::Write;
 
 const PROMPT: &str = ">> ";
+
+const MONKEY_FACE: &str = r#"          __,__
+  .--.  .-"     "-.  .--.
+ / .. \/  .-. .-.  \/ .. \
+| |  ' /   Y   Y   \  ' | |
+ \  \  \ 0 | 0 /  /  /  /
+  \ '-.\.-"""""""-./.-' /
+   '-._'-.       .-'_.-'
+"#;
 
 pub fn start<R: BufRead, W: Write>(reader: &mut R, writer: &mut W) {
     loop {
@@ -19,6 +29,21 @@ pub fn start<R: BufRead, W: Write>(reader: &mut R, writer: &mut W) {
         }
 
         let mut lexer = Lexer::new(line);
+        let mut parser = Parser::new(lexer.clone());
+        if let Some(program) = Parser::parse_program(&mut parser) {
+            if parser.errors.is_empty() {
+                writer
+                    .write_all(format!("{program:?}\n").as_bytes())
+                    .expect("failed to write program");
+            } else {
+                for err in &parser.errors {
+                    writer.write_all(MONKEY_FACE.as_bytes()).expect("monkey");
+                    writer
+                        .write_all(format!("\t{err}\n").as_bytes())
+                        .expect("failed to write error");
+                }
+            }
+        }
         loop {
             let tok = lexer.next_token();
             if tok.token_type == TokenType::Eof {
